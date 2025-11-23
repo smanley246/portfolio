72179caa754430a9ccbff1d4a3ef087a6daa7070
Samuel Manley <smanley246@gmail.com>
2025-11-23 04:47:58 -0500

    Analytics Tracking. Error fixed.

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@
  */
 
 import React from 'react';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Portfolio from './Portfolio';
